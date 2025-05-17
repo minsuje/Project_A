164aa4d764e6acb670f7bae971353d8bcaa5c2f7
minsuje <minsu.jex@gmail.com>
2025-05-17 20:10:28 +0900

    test

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,21 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+# SQlite 설정 수정
+from sqlalchemy import MetaData
+
 import config
+
+# SQLite 설정 수정
+naming_convetion = {
+    "ix": "ix_%(column_0_lable)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convetion))
+migrate = Migrate()
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -14,16 +28,25 @@ def create_app():
 
     # ORM
     db.init_app(app)
-    migrate.init_app(app, db)
+    if app.config["SQLALCHEMY_DATABASE_URI"].startswith("sqlite"):
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
+
     # model
     from .models import models
 
+    from .filter import format_datetime
+
+    app.jinja_env.filters["datetime"] = format_datetime
+
     # blueprint
-    from .views import main_views, question_views, answer_views
+    from .views import main_views, question_views, answer_views, auth_views
 
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
+    app.register_blueprint(auth_views.bp)
 
     return app
 
